6509b0cb256e45dfaffa48651ea1a6197e76dbf0
Zwilli4 <99517778+Zwilli4@users.noreply.github.com>
2026-05-26 23:04:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const incidents = [
+
+    {
+        vehicle_id: "WAYMO_1001",
+        location: "Broadway & 5th Ave",
+        severity: "High",
+        incident_type: "traffic_block"
+    },
+
+    {
+        vehicle_id: "WAYMO_1002",
+        location: "Music Row",
+        severity: "Medium",
+        incident_type: "sensor_failure"
+    },
+
+    {
+        vehicle_id: "WAYMO_1003",
+        location: "The Gulch",
+        severity: "Low",
+        incident_type: "unsafe_stop"
+    }
+
+];
+
+document.getElementById("activeIncidents").innerText = incidents.length;
+
+document.getElementById("vehicleCount").innerText =
+    new Set(incidents.map(i => i.vehicle_id)).size;
+
+document.getElementById("highSeverity").innerText =
+    incidents.filter(i => i.severity === "High").length;
+
+const table = document.getElementById("incidentTable");
+
+incidents.forEach(incident => {
+
+    const row = `
+        <tr>
+            <td>${incident.vehicle_id}</td>
+            <td>${incident.location}</td>
+            <td>${incident.severity}</td>
+            <td>${incident.incident_type}</td>
+        </tr>
+    `;
+
+    table.innerHTML += row;
+});
